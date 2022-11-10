6576379e39a17b00008b44217acdba14bc30fdbb
dima <kostiuk.dmytro.kd@gmail.com>
2022-11-10 11:46:21 +0200

    firs commit

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+console.log(1);
+let number = 5;
+
+// alert('Hi')
+
+// const answer = [];
+// answer[0] = prompt('Name?', '')
+// answer[1] = prompt('Age?', '')
+// answer[2] = prompt('Are you gay?', '')
+
+// document.write(answer);
